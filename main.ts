5bb701080a7bc5c27c0b745bcd2796f5731f6b70
fernandocode <lealfernandoleal@gmail.com>
2018-01-12 08:57:46 -0200

    add export members

--- main.ts
+++ main.ts
@@ -2,7 +2,18 @@ export * from "./src/crud/crud";
 export * from "./src/crud/delete/delete";
 export * from "./src/crud/update/update";
 export * from "./src/crud/insert/insert";
-export * from "./src/crud/query/query";
+export * from "./src/crud/query";
 export * from "./src/ddl/ddl";
 export * from "./src/ddl/create/create";
 export * from "./src/ddl/drop/drop";
+
+export { IGetMapper } from "./src/definitions/interface-get-mapper";
+export { ResultExecuteSql, QueryCompiled } from "./src/core/utils";
+export { ExecutableBuilder } from './src/core/executable-builder';
+export { DatetimeUtils } from './src/datetime-utils';
+export { DatabaseHelper } from "./src/database-helper";
+export { JoinType } from './src/crud/query/query-builder';
+export { WhereBuilder } from './src/crud/where-builder';
+export { MetadataTable } from './src/metadata-table';
+
+// export * from "./src/";
